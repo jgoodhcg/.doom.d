17dff294ebd020627059b954c3edf659d78706f1
Justin Good <jgoodhcg@gmail.com>
2023-12-16 15:44:57 -0500

    Synced up m3 mackbook install of doom

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -15,20 +15,16 @@
 ;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
 ;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-symbol-font' -- for symbols
 ;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 24 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 26))
-
-;; For some reason `doom-variable-pitch-font' breaks doom on mac
-(setq doom-font     (font-spec :family "Fira Code" :size 24 :weight 'regular)
-      doom-big-font (font-spec :family "Fira code" :size 48 :weight 'regular))
-
-
+(setq doom-font (font-spec :family "Fira Code" :size 24 :weight 'semi-light)
+      doom-big-font (font-spec :family "Fira Code" :size 48 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 24))
+;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
